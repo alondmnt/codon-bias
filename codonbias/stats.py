cdf4b86a2aa4de914edb6c4f3cabccab841bab67
Alon Diament <alondmnt@users.noreply.github.com>
2022-08-06 21:47:46 +0300

    new: CodonCounter class

---
+++ codonbias/stats.py
@@ -0,0 +1,41 @@
+from collections import Counter, Iterable
+import os
+
+import pandas as pd
+
+gc = pd.read_csv(f'{os.path.dirname(__file__)}/genetic_code_ncbi.csv',
+                 index_col=0).sort_index()
+
+
+class CodonCounter(object):
+    def __init__(self, seqs, genetic_code=1):
+        self.genetic_code = str(genetic_code)
+        self.counts = pd.Series(self._count(seqs), name='count')
+
+    def _count(self, seqs):
+        if not isinstance(seqs, str) and isinstance(seqs, Iterable):
+            return sum(list(map(self._count, seqs)), start=Counter())
+
+        seqs = seqs.upper().replace('U', 'T')
+
+        return Counter([seqs[i:i+3] for i in range(0, len(seqs), 3)])
+
+    def get_codon_table(self, normed=False):
+        stats = gc[[self.genetic_code]].join(self.counts)\
+            .fillna(0.)['count']
+        if normed:
+            stats /= stats.sum()
+
+        return stats
+
+    def get_aa_table(self, normed=False):
+        stats = gc[[self.genetic_code]].join(self.counts)\
+            .rename(columns={self.genetic_code: 'aa'}).fillna(0.)\
+            .set_index('aa', append=True).reorder_levels(['aa', 'codon'])\
+            .sort_index()
+        if normed:
+            stats = stats.join(stats.groupby('aa').sum()\
+                .rename(columns={'count': 'norm'}))
+            stats['count'] /= stats['norm']
+
+        return stats['count']
