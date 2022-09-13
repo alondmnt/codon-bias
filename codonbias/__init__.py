a28cf96e9418a875da3de254c58a8215498d168c
Alon Diament <alondmnt@users.noreply.github.com>
2022-09-13 18:43:56 +0300

    docs: package docstring

--- codonbias/__init__.py
+++ codonbias/__init__.py
@@ -4,18 +4,25 @@ protein coding regions, translation efficiency and synonymous mutations.
 These include implementations of popular models from the past four decades
 of codon usage study, such as:
 
+- Nucleotide and codon k-mer statistics (GC, GC3, CpG, etc.)
 - Frequency of Optimal Codons (FOP)
 - Relative Synonymous Codon Usage (RSCU)
-- Codon Adaptation Index (CAI)
-- Effective Number of Codons (ENC)
+- Codon Adaptation Index (CAI), including extensions:
+    - Codon pair (and k-mers) adaptation
+- Effective Number of Codons (ENC), including extensions:
+    - Background correction
+    - Improved estimation
+    - Effective number of codon pairs (and k-mers) (ENcp)
 - tRNA Adaptation Index (tAI)
+- Codon Pair Bias (CPB/CPS)
 - Relative Codon Bias Score (RCBS)
+- Normalized Translational Efficiency (nTE)
 - Directional Codon Bias Score (DCBS)
 - Codon Usage Frequency Similarity (CUFS)
 
 The package contains 4 submodules:
 
-- codonbias.stats: Classes for codon statistics.
+- codonbias.stats: Classes for basepair / codon statistics.
 - codonbias.scores: Models / scores that operate on individual sequences
   independently.
 - codonbias.pairwise: Models / scores that operate on pairs of sequences.
